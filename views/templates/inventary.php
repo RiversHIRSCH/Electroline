a8183c4b24f50cfcd13c51b1de10e99013a244fd
RiversHIRSCH <61602942+RiversHIRSCH@users.noreply.github.com>
2020-07-18 12:20:16 -0500

    agregar inventario

--- views/templates/inventary.php
+++ views/templates/inventary.php
@@ -1,10 +1,159 @@
+<!-- Modal Structure -->
+<div id="modal1" class="modal">
+    <div class="modal-content">
+        <div class="row">
+            <h4 class="center grey-text"><strong>AGREGAR</strong></h4>
+            <form>
+                <div class="input-field col s6">
+                    <input id="nombreProducto" type="text" class="validate" required>
+                    <label for="nombreProducto">Nombre</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="marcaProducto" type="text" class="validate" required>
+                    <label for="marcaProducto">Marca</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="precioProducto" type="number" class="validate" required>
+                    <label for="precioProducto">Precio</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="categoriaProducto" type="text" class="validate" value="Audio" disabled>
+                    <label for="categoriaProducto">Categoria</label>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Guardar</a>
+    </div>
+</div>
+<div id="modal2" class="modal">
+    <div class="modal-content">
+        <div class="row">
+            <h4 class="center grey-text"><strong>AGREGAR</strong></h4>
+            <form>
+                <div class="input-field col s6">
+                    <input id="nombreProducto2" type="text" class="validate" required>
+                    <label for="nombreProducto2">Nombre</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="marcaProducto2" type="text" class="validate" required>
+                    <label for="marcaProducto2">Marca</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="precioProducto2" type="number" class="validate" required>
+                    <label for="precioProducto2">Precio</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="categoriaProducto2" type="text" class="validate" value="Cableado" disabled>
+                    <label for="categoriaProducto2">Categoria</label>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Guardar</a>
+    </div>
+</div>
+<div id="modal3" class="modal">
+    <div class="modal-content">
+        <div class="row">
+            <h4 class="center grey-text"><strong>AGREGAR</strong></h4>
+            <form>
+                <div class="input-field col s6">
+                    <input id="nombreProducto3" type="text" class="validate" required>
+                    <label for="nombreProducto3">Nombre</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="marcaProducto3" type="text" class="validate" required>
+                    <label for="marcaProducto3">Marca</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="precioProducto3" type="number" class="validate" required>
+                    <label for="precioProducto3">Precio</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="categoriaProducto3" type="text" class="validate" value="Ilumunación" disabled>
+                    <label for="categoriaProducto3">Categoria</label>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Guardar</a>
+    </div>
+</div>
+<div id="modal4" class="modal">
+    <div class="modal-content">
+        <div class="row">
+            <h4 class="center grey-text"><strong>AGREGAR</strong></h4>
+            <form>
+                <div class="input-field col s6">
+                    <input id="nombreProducto4" type="text" class="validate" required>
+                    <label for="nombreProducto4">Nombre</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="marcaProducto4" type="text" class="validate" required>
+                    <label for="marcaProducto4">Marca</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="precioProducto4" type="number" class="validate" required>
+                    <label for="precioProducto4">Precio</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="categoriaProducto4" type="text" class="validate" value="Componentes" disabled>
+                    <label for="categoriaProducto4">Categoria</label>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Guardar</a>
+    </div>
+</div>
+<div id="modal5" class="modal">
+    <div class="modal-content">
+        <div class="row">
+            <h4 class="center grey-text"><strong>AGREGAR</strong></h4>
+            <form>
+                <div class="input-field col s6">
+                    <input id="nombreProducto5" type="text" class="validate" required>
+                    <label for="nombreProducto5">Nombre</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="marcaProducto5" type="text" class="validate" required>
+                    <label for="marcaProducto5">Marca</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="precioProducto5" type="number" class="validate" required>
+                    <label for="precioProducto5">Precio</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="categoriaProducto5" type="text" class="validate" value="Ventas" disabled>
+                    <label for="categoriaProducto5">Categoria</label>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Guardar</a>
+    </div>
+</div>
+
 <div class="row">
     <div class="col s12 m9 l10" style="overflow-y: hidden; height: 92vh">
         <div id="seccionAudio" class="section" style="padding-left: 5vh;">
             <div class="row">
                 <br>
-                <div class="col l2 offset-l5">
-                    <h4 style="border-left: 5px solid #e57373; padding-left: 3vh;">Audio</h4>
+                <div class="col l4 offset-l4">
+                    <div class="row valign-wrapper">
+                        <div class="col l6">
+                            <h4 style="border-left: 5px solid #e57373; padding-left: 3vh;">Audio</h4>
+                        </div>
+                        <div class="col l6" style="padding-top: 3vh;">
+                            <button class="btn-small red lighten-2 waves-effect waves-light btn modal-trigger" href="#modal1">Agregar</button>
+                        </div>
+                    </div>
                 </div>
                 <div class="col l12">
                     <table id="tablaAudio" class="order-column hover nowrap compact" style="width: 100%;">
@@ -146,8 +295,15 @@
         <div id="seccionCableado" class="section" style="padding-left: 5vh;">
             <div class="row">
                 <br>
-                <div class="col l2 offset-l5">
-                    <h4 style="border-left: 5px solid #e57373; padding-left: 3vh;">Cableado</h4>
+                <div class="col l4 offset-l4">
+                    <div class="row valign-wrapper">
+                        <div class="col l6">
+                            <h4 style="border-left: 5px solid #e57373; padding-left: 3vh;">Cableado</h4>
+                        </div>
+                        <div class="col l6" style="padding-top: 3vh;">
+                            <button class="btn-small red lighten-2 waves-effect waves-light btn modal-trigger" href="#modal2">Agregar</button>
+                        </div>
+                    </div>
                 </div>
                 <div class="col l12">
                     <table id="tablaCableado" class="order-column hover nowrap compact" style="width: 100%;">
@@ -289,8 +445,15 @@
         <div id="seccionIluminación" class="section" style="padding-left: 5vh;">
             <div class="row">
                 <br>
-                <div class="col l2 offset-l5">
-                    <h4 style="border-left: 5px solid #e57373; padding-left: 3vh;">Iluminación</h4>
+                <div class="col l5 offset-l4">
+                    <div class="row valign-wrapper">
+                        <div class="col l6">
+                            <h4 style="border-left: 5px solid #e57373; padding-left: 3vh;">Iluminación</h4>
+                        </div>
+                        <div class="col l6" style="padding-top: 3vh;">
+                            <button class="btn-small red lighten-2 waves-effect waves-light btn modal-trigger" href="#modal3">Agregar</button>
+                        </div>
+                    </div>
                 </div>
                 <div class="col l12">
                     <table id="tablaIluminacion" class="order-column hover nowrap compact" style="width: 100%;">
@@ -432,8 +595,15 @@
         <div id="seccionComponentes" class="section" style="padding-left: 5vh;">
             <div class="row">
                 <br>
-                <div class="col l2 offset-l5">
-                    <h4 style="border-left: 5px solid #e57373; padding-left: 3vh;">Componentes</h4>
+                <div class="col l6 offset-l4">
+                    <div class="row valign-wrapper">
+                        <div class="col l6">
+                            <h4 style="border-left: 5px solid #e57373; padding-left: 3vh;">Componentes</h4>
+                        </div>
+                        <div class="col l6" style="padding-top: 3vh;">
+                            <button class="btn-small red lighten-2 waves-effect waves-light btn modal-trigger" href="#modal4">Agregar</button>
+                        </div>
+                    </div>
                 </div>
                 <div class="col l12">
                     <table id="tablaComponentes" class="order-column hover nowrap compact" style="width: 100%;">
