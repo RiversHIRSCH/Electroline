db9eb1b9174da81811a84acfd13652bfe76d48a2
RiversHIRSCH <61602942+RiversHIRSCH@users.noreply.github.com>
2020-08-12 14:16:54 -0500

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Electronline</title>
     <!--Import Google Icon Font-->
     <link href="views\static\css\fallback-icons.css" rel="stylesheet">
     <!-- jQuery -->
